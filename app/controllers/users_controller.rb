ef29f2390749ad1c69f3e1091223e9f0b96f7796
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-12-15 17:14:00 -0500

    commit tree 181244659b59df58168341fe5a9e4c9ec2baa9a4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def show
+        user_items = User.find(params[:id])
+        render json: user_items, include: :items, status: :ok   
+     end
+
 end
